6b65f1a1b6928a3ea61ea7f206266cec8d1ef9b1
tera <omegal@hotmail.co.jp>
2015-02-27 06:59:57 +0900

    スクロールの判定処理完了

--- app/src/main/java/com/fefeyo/nikcg/BattleDetailActivity.java
+++ app/src/main/java/com/fefeyo/nikcg/BattleDetailActivity.java
@@ -2,14 +2,70 @@ package com.fefeyo.nikcg;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.AbsListView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import com.fefeyo.nikcg.data.DetailListItem;
+
+import java.util.ArrayList;
 
 
 public class BattleDetailActivity extends ActionBarActivity {
+   private ImageView leftFace;
+   private ImageView rightFace;
+   private SeekBar battleBar;
+   private ListView listView;
+   private DetailAdapter detailAdapter;
+   private ArrayList<DetailListItem> item;
+   private int position;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle_detail);
+
+       leftFace = (ImageView)findViewById(R.id.leftFace);
+       rightFace = (ImageView)findViewById(R.id.rightFace);
+        battleBar = (SeekBar)findViewById(R.id.battleBar);
+        listView = (ListView)findViewById(R.id.listView);
+
+        detailAdapter =new DetailAdapter(this,
+                    0,
+                   item );
+        listView.setAdapter(detailAdapter);
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                  position = listView.getChildAt(0).getTop();
+                if(firstVisibleItem + visibleItemCount == totalItemCount) {
+                    //勝敗処理
+                }else {
+                    //ターンが終わった場合
+                    if ((firstVisibleItem + visibleItemCount - 1) % 2 == 0) {
+
+
+                    }
+                }
+
+            }
+        });
+
+
+
+
+
+
+
 
 
 
