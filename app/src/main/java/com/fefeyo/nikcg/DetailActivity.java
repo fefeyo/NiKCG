75702ed22702f592daba6628571c032fddda4091
tera <omegal@hotmail.co.jp>
2015-02-27 15:13:19 +0900

    JSONLoder

--- app/src/main/java/com/fefeyo/nikcg/DetailActivity.java
+++ app/src/main/java/com/fefeyo/nikcg/DetailActivity.java
@@ -3,8 +3,6 @@ package com.fefeyo.nikcg;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -47,25 +45,5 @@ public class DetailActivity extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
