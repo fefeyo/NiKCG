e56185a7aefafd0c5a334d8a726f44f617e73b89
tera <omegal@hotmail.co.jp>
2015-02-27 13:44:07 +0900

    詳細画面

--- app/src/main/java/com/fefeyo/nikcg/DetailActivity.java
+++ app/src/main/java/com/fefeyo/nikcg/DetailActivity.java
@@ -1,9 +1,19 @@
 package com.fefeyo.nikcg;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class DetailActivity extends ActionBarActivity {
@@ -12,7 +22,33 @@ public class DetailActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        AssetManager assetManager = getResources().getAssets();
+        InputStream inputStream = null;
+        BufferedReader bufferedReader = null;
 
+        try {
+            // 事前に用意しておいた、山手線の駅情報を読み込みます
+            inputStream = assetManager.open("http://dac2.snnm.net:80/api/search?" +
+                    "keyword=%E6%94%BF%E6%B2%BB%E3%81%A8%E3%82%AB%E3%83%8D%E3%81%A7%E8" +
+                    "%B3%AA%E5%95%8F%E6%94%BB%E5%8B%A2&volume=1");
+            bufferedReader =
+                    new BufferedReader(new InputStreamReader(inputStream));
+            String str = bufferedReader.readLine();
+
+            // JSONObject に変換します
+            JSONObject json = new JSONObject(str);
+
+            // JSONObject を文字列に変換してログ出力します
+            Log.d("Detail", json.toString());
+
+            inputStream.close();
+            bufferedReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 
