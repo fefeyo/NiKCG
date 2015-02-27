03ae80dd66af2bfc903d81dfea4932ca64aa1456
tera <omegal@hotmail.co.jp>
2015-02-27 11:17:44 +0900

    直し

--- app/src/main/java/com/fefeyo/nikcg/BattleDetailActivity.java
+++ app/src/main/java/com/fefeyo/nikcg/BattleDetailActivity.java
@@ -3,7 +3,6 @@ package com.fefeyo.nikcg;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -34,51 +33,6 @@ public class BattleDetailActivity extends ActionBarActivity implements AdapterVi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle_detail);
-//
-//       leftFace = (ImageView)findViewById(R.id.leftFace);
-//       rightFace = (ImageView)findViewById(R.id.rightFace);
-//        battleBar = (ProgressBar)findViewById(R.id.ProBar);
-//        listView = (ListView)findViewById(R.id.listView);
-//        leftDamage = (TextView)findViewById(R.id.leftDamage);
-//        rightDamage = (TextView)findViewById(R.id.rightDamage);
-//        progress = 50;
-//        battleBar.setMax(MAX);
-//        battleBar.setProgress(progress);
-
-
-        detailAdapter =new DetailAdapter(this,
-                    0,
-                   rightItem,
-                   leftItem
-                );
-        listView.setAdapter(detailAdapter);
-        listView.setOnItemClickListener(this);
-
-        scrollFrag = 2;
-
-        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-
-            }
-
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                  position = listView.getChildAt(0).getTop();
-                if(firstVisibleItem + visibleItemCount == totalItemCount) {
-                    //勝敗処理
-                    if(battleBar.getProgress() < 50){
-                        //左側が勝った場合
-
-                    }else if(battleBar.getProgress() > 50){
-                       //右側が勝った場合
-                    }else {
-                        //引き分けの場合
-                    }
-                }
-
-            }
-        });
 
     }
 
